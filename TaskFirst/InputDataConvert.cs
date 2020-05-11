c5f1b32319861c6826d15682318045a12b6c3e9a
arturmekler <artur.mekler@gmail.com>
2020-05-11 23:05:57 +0200

    Initial inputDataConvert

--- TaskFirst/InputDataConvert.cs
+++ TaskFirst/InputDataConvert.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskFirst.Model;
 
 namespace TaskFirst
 {
@@ -11,19 +12,59 @@ namespace TaskFirst
     {
         public static void ReadTxtFile()
         {
+            string line;
+            int counter = 0;
             try
-            {   // Open the text file using a stream reader.
-                using (StreamReader sr = new StreamReader("InputData.txt"))
+            {
+                using (StreamReader file = new StreamReader("InputData.txt"))
                 {
-                    // Read the stream to a string, and write the string to the console.
-                    String line = sr.ReadToEnd();
-                    Console.WriteLine(line);
+                    while ((line = file.ReadLine()) != null)
+                    {
+
+                        GenerateAllMaterials(line);
+                        counter++;
+                    }
+
+                    file.Close();
                 }
             }
             catch (IOException e)
             {
                 Console.WriteLine("The file could not be read:");
                 Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void GenerateAllMaterials(string line)
+        {
+            if (line.FirstOrDefault() == '#' || String.IsNullOrEmpty(line))
+            {
+
+            }
+            else
+            {
+                Console.WriteLine(line);
+                var splitedLine = line.Split(';');
+
+
+                BusinessObject businessObject = new BusinessObject()
+                {
+                    Name = splitedLine?.FirstOrDefault(),
+                    ID = splitedLine?.ElementAt(1)
+                };
+
+                var magazine = splitedLine.ElementAt(2)?.Split('|');
+
+
+                foreach(var m in magazine)
+                {
+                    var countElementInMagazine = m.Split(',').ElementAt(1);
+
+                    Magazine mag = new Magazine()
+                    {
+                        Name = m.Split(',').FirstOrDefault()
+                    };
+                }
             }
         }
     }
