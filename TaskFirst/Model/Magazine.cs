d71e67d5ef61bcdb730bdd84915de3c6ed298740
arturmekler <artur.mekler@gmail.com>
2020-05-11 19:58:17 +0200

    Create initial model of project

---
+++ TaskFirst/Model/Magazine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFirst.Model
+{
+    class Magazine
+    {
+        public string Name { get; set; }
+        public List<BusinessObject> businessObjects;
+        public int CountOfBusinessObjects { get; set; }
+    }
+}
